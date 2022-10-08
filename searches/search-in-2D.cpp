ed775e58eb58f1465167448b740c6b1e72820df5
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-10-08 13:35:53 +0500

    Search in 2-D Matrix
    #6795

---
+++ searches/search-in-2D.cpp
@@ -0,0 +1,14 @@
+# Python code for the above approach
+def linearSearch (arr, target):
+	for i in range(len(arr)):
+		for j in range(len(arr[i])):
+			if (arr[i][j] == target):
+				return [i, j]
+	return [-1, -1]
+
+# Driver code
+arr = [[3, 12, 9], [5, 2, 89], [90, 45, 22]]
+target = 89
+ans = linearSearch(arr, target)
+print(f"Element found at index: [{ans[0]} {ans[1]}]")
+
